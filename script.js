cb0fd7432a5bab9ab01d0efd256495db26421f0d
clqkx <clqkx@proton.me>
2024-06-14 18:35:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ console.log(`${consolePrefix}Injecting...`, consoleGreen);
 
 try {
     function onClickAuto() {
-        u(`AutoBot ${P.cat.isAuto ? 'deactivated' : 'activated'}!\n\nCracked by @clqkx`)
         P.cat.isAuto = !P.cat.isAuto,
         P.cat.isAuto ? (this.ani8.play(0, !0),
         Laya.timer.loop(500, this, this.checkAuto),
-        this.checkFreeCat()) : (Laya.timer.clearAll(this.checkAuto),
+        this.checkFreeCat(),
+        this.m_btn_AutoSetting.visible = !0) : (Laya.timer.clearAll(this.checkAuto),
         this.ani8.stop(),
-        Laya.timer.loop(5e3, this, this.checkSum)),
+        Laya.timer.loop(5e3, this, this.checkSum),
+        this.m_btn_AutoSetting.visible = !1),
         this.m_img_StopAuto.visible = !P.cat.isAuto
+        u(`AutoBot ${P.cat.isAuto ? 'deactivated' : 'activated'}!\n\nCracked by @clqkx`)
     }
     
     M.prototype.onClickAuto = onClickAuto
